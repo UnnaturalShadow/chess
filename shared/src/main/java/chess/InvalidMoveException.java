f1565e243470079da04e93bbae954489457fbb1b
unnaturalshadow <joshuahdecker@gmail.com>
2025-09-13 17:24:51 -0600

    Added king move logic

--- shared/src/main/java/chess/InvalidMoveException.java
+++ shared/src/main/java/chess/InvalidMoveException.java
@@ -3,7 +3,8 @@ package chess;
 /**
  * Indicates an invalid move was made in a game
  */
-public class InvalidMoveException extends Exception {
+public class InvalidMoveException extends Exception
+{
 
     public InvalidMoveException() {}
 
