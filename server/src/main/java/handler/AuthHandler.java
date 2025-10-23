9e5f424564e9b9129fb4dca673bcfbe8b4534d6f
unnaturalshadow <joshuahdecker@gmail.com>
2025-10-22 22:01:42 -0600

    Fixed Error code misalign?

--- server/src/main/java/handler/AuthHandler.java
+++ server/src/main/java/handler/AuthHandler.java
@@ -17,9 +17,17 @@ public class AuthHandler
 
     public void logout(Context context)
     {
+        String authHeader = context.header("authorization");
+
+        if (authHeader == null || authHeader.isEmpty())
+        {
+            setErrorContext(context, "401 Unauthorized Error: Unauthorized", 401);
+            return;
+        }
+
         try
         {
-            authService.logout(context.header("authorization"));
+            authService.logout(authHeader);
         }
         catch (DataAccessException e)
         {
