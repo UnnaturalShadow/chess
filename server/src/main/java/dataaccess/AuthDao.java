21f8897a3c4d53bbea7fdd25efaa43ceaab2adf5
unnaturalshadow <joshuahdecker@gmail.com>
2025-10-22 22:08:43 -0600

    Fixed Error code misalign attempt 3

--- server/src/main/java/dataaccess/AuthDao.java
+++ server/src/main/java/dataaccess/AuthDao.java
@@ -6,6 +6,6 @@ public interface AuthDao extends Dao
 {
     void addAuthToken(String username, String token);
     String authenticateToken(String token);
-    void remove(String token) throws UserNotValidatedException;
+    void remove(String token) throws DataAccessException;
     void clear();
 }
