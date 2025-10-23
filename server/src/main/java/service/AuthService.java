82483c96cdddeeaa26f3996efb05cbcb69515640
unnaturalshadow <joshuahdecker@gmail.com>
2025-10-22 21:57:42 -0600

    Fixed compile failure.

--- server/src/main/java/service/AuthService.java
+++ server/src/main/java/service/AuthService.java
@@ -26,7 +26,7 @@ public class AuthService extends Service
         return newToken;
     }
 
-    public void logout(String token) throws UserNotValidatedException
+    public void logout(String token) throws DataAccessException, UserNotValidatedException
     {
         if (daos.authDao.authenticateToken(token) == null)
         {
