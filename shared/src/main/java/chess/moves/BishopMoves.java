dcf2397c6888323ce2e38584c26ea5e7ef37ae3b
UnnaturalShadow <joshuahdecker@gmail.com>
2026-01-08 14:34:23 -0700

    Queen logic easily extrapolates to rook and bishop, added their matrixes as well.

---
+++ shared/src/main/java/chess/moves/BishopMoves.java
@@ -0,0 +1,10 @@
+package chess.moves;
+
+public class BishopMoves
+{
+    int[][] bishopMatrix =
+            {
+                    {-1, 1}, {1, 1},
+                    {-1, -1}, {1, -1}
+            };
+}
---
+++ shared/src/main/java/chess/moves/RookMoves.java
@@ -0,0 +1,10 @@
+package chess.moves;
+
+public class RookMoves
+{
+    int[][] rookMatrix = {
+            {0,1},
+            {-1,0}, {1,0},
+            {0,-1}
+    };
+}
