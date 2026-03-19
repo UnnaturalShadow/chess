dd5df123150409f4805017607ca929a0403c0648
Shadow <joshuahdecker@gmail.com>
2026-03-19 15:31:27 -0600

    warning cleanup

--- client/src/main/java/client/ClientMain.java
+++ client/src/main/java/client/ClientMain.java
@@ -1,11 +1,10 @@
 package client;
 
 import chess.*;
-import client.ServerFacade;
 import ui.ChessClient;
 
 public class ClientMain {
-    public static void main(String[] args) {
+    static void main(String[] args) {
         ServerFacade facade = new ServerFacade(8080);
         System.out.println("Success!");
         ChessClient chessClient = new ChessClient(facade);
